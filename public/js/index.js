a574674550407730cf3fac642d6c679f35b4ea28
nedagosa-cod <nestorcode4004@gmail.com>
2023-08-07 10:13:41 -0500

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const toogleCaract = document.querySelectorAll('.btn-toggle')
+const menuMobile = document.querySelector('#menu-mobile')
 
 
 toogleCaract.forEach(el=>{
@@ -14,4 +15,12 @@ document.getElementById('next').onclick = function(){
 document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+
+
+menuMobile.onclick = ()=> {
+    menuMobile.previousElementSibling.hasAttribute('style') ?  
+        menuMobile.previousElementSibling.style.display =  'none' : 
+        menuMobile.previousElementSibling.style.display = 'flex'
+    menuMobile.classList.toggle(menuMobile.className.includes('fa-bars') ? 'fa-xmark' :  'fa-bars')
 }
